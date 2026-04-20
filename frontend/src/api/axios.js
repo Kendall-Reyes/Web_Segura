f22ccde777a25458d2f31008694011ba4d878a16
Allison Blanco Rodríguez <allisonblancorodriguez@gmail.com>
2026-04-20 16:26:15 -0600

    Se configuró axios para los response de los métodos

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -5,4 +5,16 @@ const instance = axios.create({
   withCredentials: true, // importante para cookies HttpOnly
 });
 
+instance.interceptors.response.use(
+  (response) => {
+    // Si la respuesta tiene .data.data, extráelo automáticamente
+    // Si no (como el login), devuelve response.data normal
+    if (response.data?.data !== undefined) {
+      return response.data.data;
+    }
+    return response.data;
+  },
+  (error) => Promise.reject(error)
+);
+
 export default instance;
