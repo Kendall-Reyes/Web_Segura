a992064e24edefc9516431c041e626af34d826f5
Allison Blanco Rodríguez <allisonblancorodriguez@gmail.com>
2026-04-20 16:29:49 -0600

    Correción en CreateUser

--- frontend/src/pages/user/CreateUser.jsx
+++ frontend/src/pages/user/CreateUser.jsx
@@ -1,15 +1,18 @@
 import UserForm from "../../components/users/UserForm";
 import { useNavigate } from "react-router-dom";
+import axios from "../../api/axios";
 
 export default function CreateUser() {
     const navigate = useNavigate();
 
     const handleCreate = async (data) => {
-        console.log("Crear usuario:", data);
-
-        // 🔐 luego: axios POST
-
-        navigate("/app/usuarios");
+        try {
+            const { nombre, email, password, rolId } = data;
+            await axios.post("/api/usuarios", { nombre, email, password, rolId });
+            navigate("/app/usuarios");
+        } catch (error) {
+            console.error("Error al crear usuario:", error);
+        }
     };
 
     return (
